fdc5ce5cd8c3a493d3c457d330af951b2c573ca6
Kao Saelor <saelor.kao@gmail.com>
2023-04-25 17:59:09 -0700

    create form to add/delete book

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import axios from 'axios';
-import { Carousel } from 'react-bootstrap';
+import { Carousel, Button } from 'react-bootstrap';
 import Image from './Image/bookImg.jpg'
+import BookModal from './BookForm';
 // import './App.css';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      showModal: false,
     }
   }
 
@@ -32,9 +34,62 @@ class BestBooks extends React.Component {
     }
   };
 
+  // prevent default
+  handleBookSubmit = (event) => {
+    event.preventDefault();
+
+    // construct a book obj based on the form input values
+    let bookObj = {
+      title: event.target.title.value,
+      description: event.target.description.value,
+      status: event.target.status.checked
+    }
+    console.log(bookObj);
+
+    // Create add book 
+    const postBook = async (bookObj) => {
+      try {
+        let url = `${process.env.REACT_APP_SERVER}/books`
+        let postBook = await axios.post(url, bookObj);
+        this.setState({ books: [...this.state.books, postBook.data] })
+        this.closeModal();
+
+        console.log('Book saved', postBook.data)
+
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    postBook(bookObj);
+  };
+
+
+  // create delete book
+  deleteBook = async (bookID) => {
+    try {
+      let url = `${process.env.REACT_APP_SERVER}/books/${bookID}`;
+      await axios.delete(url);
+      let updatedBooks = this.state.books.filter(book => book._id !== bookID)
+      this.setState({ books: updatedBooks });
+
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
   componentDidMount() {
     this.getBooks();
   }
+
+  // create the modal  
+  openModal = () => {
+    this.setState({ showModal: true })
+  };
+
+  closeModal = () => {
+    this.setState({ showModal: false })
+  };
+
 
   render() {
 
@@ -55,8 +110,18 @@ class BestBooks extends React.Component {
                   style={{ maxHeight: '500px', objectFit: 'contain', marginTop: '2em', marginBottom: '2em' }}
                 />
                 <Carousel.Caption>
-                  <h3>{book.title}</h3>
-                  <p>{book.description}</p>
+                  <h3 className='book-title'>{book.title}</h3>
+                  <h5> {book.status && (
+                    <>
+                      <span role="img" aria-label="heart"> ❤️ </span>
+                      <strong><em>Highly Recommended</em></strong>
+                    </>
+                  )}</h5>
+                  <p className='book-description'>{book.description}</p>
+                  <div className='button-div'>
+                    <Button className='add-button' onClick={this.openModal}>Add a Book!</Button>
+                    <Button className='delete-button' onClick={() => this.deleteBook(book._id)}>Delete a Book!</Button>
+                  </div>
                 </Carousel.Caption>
               </Carousel.Item>
             ))}
@@ -64,6 +129,14 @@ class BestBooks extends React.Component {
         ) : (
           <h3>No Books Found :(</h3>
         )}
+
+        <BookModal
+          show={this.state.showModal}
+          onHide={this.closeModal}
+          onSubmit={this.handleBookSubmit}
+          onBookAdd={this.closeModal}
+        />
+
       </>
     );
   }
